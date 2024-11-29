db1f858ca559c107382ff0a9942c7bf5b740fcc4
Coosanta <coosantamapy@gmail.com>
2024-11-29 19:29:22 +1100

    Initial commit

---
+++ src/main/java/net/coosanta/metal_pipe/init/CoosantaMetalPipeModItems.java
@@ -0,0 +1,20 @@
+
+/*
+ *    MCreator note: This file will be REGENERATED on each build.
+ */
+package net.coosanta.metal_pipe.init;
+
+import net.minecraftforge.registries.RegistryObject;
+import net.minecraftforge.registries.ForgeRegistries;
+import net.minecraftforge.registries.DeferredRegister;
+
+import net.minecraft.world.item.Item;
+
+import net.coosanta.metal_pipe.CoosantaMetalPipeMod;
+
+public class CoosantaMetalPipeModItems {
+	public static final DeferredRegister<Item> REGISTRY = DeferredRegister.create(ForgeRegistries.ITEMS, CoosantaMetalPipeMod.MODID);
+	public static final RegistryObject<Item> METAL_PIPE = REGISTRY.register("metal_pipe", () -> new MetalPipeItem());
+	// Start of user code block custom items
+	// End of user code block custom items
+}
---
+++ src/main/java/net/coosanta/metal_pipe/item/MetalPipeItem.java
@@ -0,0 +1,50 @@
+
+package net.coosanta.metal_pipe.item;
+
+import net.minecraft.world.level.Level;
+import net.minecraft.world.item.crafting.Ingredient;
+import net.minecraft.world.item.TooltipFlag;
+import net.minecraft.world.item.Tier;
+import net.minecraft.world.item.SwordItem;
+import net.minecraft.world.item.Items;
+import net.minecraft.world.item.ItemStack;
+import net.minecraft.world.item.Item;
+import net.minecraft.network.chat.Component;
+
+import java.util.List;
+
+public class MetalPipeItem extends SwordItem {
+	public MetalPipeItem() {
+		super(new Tier() {
+			public int getUses() {
+				return 500;
+			}
+
+			public float getSpeed() {
+				return 4f;
+			}
+
+			public float getAttackDamageBonus() {
+				return 0f;
+			}
+
+			public int getLevel() {
+				return 2;
+			}
+
+			public int getEnchantmentValue() {
+				return 15;
+			}
+
+			public Ingredient getRepairIngredient() {
+				return Ingredient.of(new ItemStack(Items.IRON_INGOT));
+			}
+		}, 3, -3f, new Item.Properties());
+	}
+
+	@Override
+	public void appendHoverText(ItemStack itemstack, Level level, List<Component> list, TooltipFlag flag) {
+		super.appendHoverText(itemstack, level, list, flag);
+		list.add(Component.literal("Bonk!"));
+	}
+}
